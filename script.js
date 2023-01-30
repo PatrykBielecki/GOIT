e2412b4f5cbc45405023ea54939362d8a463827b
@patryk.bielecki <patryk.bielecki@nexpertis.pl>
2023-01-30 21:05:26 +0100

    przyciski informacje o paczce

--- script.js
+++ script.js
@@ -8,24 +8,42 @@ function myColorChange() {
   element.style.color = "red";
 }
 
+const przesylka = {
+  name: "",
+  ulica: "",
+};
 
-function mojaFunkcja(imie) {
+const przesylka2 = {
+  name: "",
+  ulica: "",
+};
 
-    const clients = ["Mango", "Poly", "Ajax"];
-    const clientNameToFind = "Patryk";
-    let message;
+przesylka2.waga = 20;
+przesylka.name = "List";
+przesylka2.name = "Paczka";
+
+
+function mojaFunkcja(id) {
+
+  const element = document.getElementById("id01");
+  const element2 = document.getElementById("id02");
+
+  if (id === 1) { //instrukcje dla przycisku z informacja o przesylce 1
+
+    informacja = "Typ twojej przesylki to: " + przesylka.name;
+    element.innerHTML = informacja;
     
-    for (const client of clients) {
-    
-      if (client === imie) {
-        message = "Klient z takim imieniem jest w bazie danych!";
-        break;
-      }
-    
-      message = "Nie znaleźliśmy takiego klienta w bazie danych!";
-    }
-    
-    console.log(message);
-    
+    informacja = "Przesyłka nie posiada wagi";
+    element2.innerHTML = informacja;
+  }
+
+  else if (id === 2) { //instrukcje dla przycisku z informacja o przesylce 2
+
+    informacja = "Typ twojej przesylki to: " + przesylka2.name;
+    element.innerHTML = informacja;
+  
+    informacja = "Waga twojej przesylki to: " + przesylka2.waga + " kg";
+    element2.innerHTML = informacja;
+  }
 
 }
